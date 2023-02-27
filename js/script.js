b94626c3219fd49291b96465356ee27c8a081cff
Uday <58341470+im-usb@users.noreply.github.com>
2023-02-28 00:40:52 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -22,25 +22,28 @@ const row3 = document.getElementById("row-3");
 slider.addEventListener("input", () => {
   const value = slider.value;
   if (value == 1) {
+    body.style.backgroundColor = "#3a4764";
     document.querySelector("h1").style.color = "#fefffd";
     document.querySelector("label").style.color = "#fefffd";
     document.querySelector("p").style.color = "#fefffd";
     document.getElementById("span1").style.color = "#fefffd";
     document.getElementById("span2").style.color = "#fefffd";
-    body.style.backgroundColor = "#3a4764";
-    row2.style.backgroundColor = "#181f32"; // set the background color of row-2 to light grey
-    displayStyle.style.backgroundColor = "#181f32"; // set the background color of the display to light grey
-    row3.style.backgroundColor = "#252d44"; // set the background color of row-3 to medium grey
+    row2.style.backgroundColor = "#181f32"; 
+    displayStyle.style.backgroundColor = "#181f32";
+    displayStyle.style.color = "#fefffd";
+    row3.style.backgroundColor = "#252d44"; 
   } else if (value == 2) {
     body.style.backgroundColor = "#e6e6e6";
+    
     document.querySelector("h1").style.color = "#000000";
     document.querySelector("label").style.color = "#000000";
     document.querySelector("p").style.color = "#000000";
     document.getElementById("span1").style.color = "#000000";
     document.getElementById("span2").style.color = "#000000";
-    row2.style.backgroundColor = "#ededed"; // set the background color of row-2 to medium grey
-    displayStyle.style.backgroundColor = "#ededed"; // set the background color of the display to medium grey
-    row3.style.backgroundColor = "#d1cccc"; // set the background color of row-3 to dark grey
+    row2.style.backgroundColor = "#ededed"; 
+    displayStyle.style.backgroundColor = "#ededed"; 
+    displayStyle.style.color = "#000000";
+    row3.style.backgroundColor = "#d1cccc"; 
   } else {
     document.querySelector("h1").style.color = "#fefffd";
     document.querySelector("label").style.color = "#fefffd";
@@ -48,38 +51,17 @@ slider.addEventListener("input", () => {
     document.getElementById("span1").style.color = "#fefffd";
     document.getElementById("span2").style.color = "#fefffd";
     body.style.backgroundColor = "#160628";
-    row2.style.backgroundColor = "#1d0934"; // set the background color of row-2 to dark grey
-    displayStyle.style.backgroundColor = "#1d0934"; // set the background color of the display to dark grey
-    row3.style.backgroundColor = "#1d0934"; // set the background color of row-3 to a lighter shade of dark grey
+    row2.style.backgroundColor = "#1d0934"; 
+    displayStyle.style.backgroundColor = "#1d0934";
+    displayStyle.style.color = "#fefffd";
+    row3.style.backgroundColor = "#1d0934"; 
   }
 });
-
-// slider.addEventListener("input", () => {
-//   const value = slider.value;
-//   const body = document.querySelector("body");
-//   if (value == 1) {
-//     body.style.background = '#3a4764';
-//     document.querySelector('row-2').style.background = '#181f32';
-//     document.querySelector('display').style.background = '#181f32';
-//     document.querySelector('row-3').style.background = '#252d44'; // set the background color to light grey
-//   } else if (value == 2) {
-//     body.style.background = '#3a4764';
-//     document.querySelector('row-2').style.background = '#e6e6e6';
-//     document.querySelector('display').style.background = '#181f32';
-//     document.querySelector('row-3').style.background = '#252d44'; // set the background color to medium grey
-//   } else {
-//     body.style.background = '#3a4764';
-//     document.getElementById('row-2').style.background = '#181f32';
-//     document.getElementById('display').style.background = '#181f32';
-//     document.getElementById('row-3').style.background = '#252d44'; // set the background color to dark grey
-//   }
-// });
 
 // add event listeners to each button to update the display
 symBtns.forEach(btn => {
   btn.addEventListener("click", () => {
     operator = btn.value;
-    // octToInt(display.value);
     console.log(operator);
     switch (operator) {
         case "+":
@@ -105,7 +87,7 @@ numBtns.forEach(btn => {
   btn.addEventListener("click", () => {
     operator = btn.value;
     display.value += operator;
-    console.log(operator);
+    // console.log(operator);
   });
 });
 
@@ -118,10 +100,17 @@ deleteBtn.addEventListener("click", () => {
 });
 
 equalsBtn.addEventListener("click", () => {
-  display.value = eval(display.value);
+  // display.value = eval(display.value);
+  if (display.value === ''){
+    display.value = '';
+  }
+
+  else {
+    try {
+        display.value = eval(display.value);
+    } 
+    catch {
+        display.value = "";
+    }
+  }
 });
-
-
-
-
-
